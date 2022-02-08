25b6090965029d8d51e263cc5e6fc07deae202e4
appharu pushpa <appharupushpa@outlook.com>
2022-02-08 13:16:29 +0545

    completed file upload in foler

--- backend/Helpers/StorageService/LocalFileUploadService.cs
+++ backend/Helpers/StorageService/LocalFileUploadService.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -6,23 +8,60 @@ namespace backend.Helpers.StorageService
 {
     public class LocalFileUploadService : IStorageService
     {
-        public LocalFileUploadService(IWebHostEnvironment env)
+        private readonly IWebHostEnvironment _env;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public LocalFileUploadService(IWebHostEnvironment env, IHttpContextAccessor httpContextAccessor)
         {
+            _httpContextAccessor = httpContextAccessor;
+            _env = env;
 
         }
+
+
+        public async Task<string> EditFile(string containerName, IFormFile file, string fileRoute)
+        {
+            await DeleteFile(fileRoute, containerName);
+            return await SaveFile(containerName, file);
+        }
+
         public Task DeleteFile(string fileRoute, string containerName)
         {
-            throw new System.NotImplementedException();
+            if (string.IsNullOrEmpty(fileRoute))
+            {
+                return Task.CompletedTask;
+            }
+            var fileName = Path.GetFileName(fileRoute);
+            var fileDirectory = Path.Combine(_env.WebRootPath, containerName, fileName);
+
+            if (File.Exists(fileDirectory))
+            {
+                File.Delete(fileDirectory);
+            }
+
+            return Task.CompletedTask;
         }
 
-        public Task<string> EditFile(string containerName, IFormFile file, string fileRoute)
+        public async Task<string> SaveFile(string container, IFormFile file)
         {
-            throw new System.NotImplementedException();
-        }
+            var extension = Path.GetExtension(file.FileName);
+            var fileName = $"{Guid.NewGuid()}{extension}";
+            string folder = Path.Combine(_env.WebRootPath, container);
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
 
-        public Task<string> SaveFile(string container, IFormFile file)
-        {
-            throw new System.NotImplementedException();
+            string route = Path.Combine(folder, fileName);
+            using (var ms = new MemoryStream())
+            {
+                await file.CopyToAsync(ms);
+                var content = ms.ToArray();
+                await File.WriteAllBytesAsync(route, content);
+
+            }
+            var url = $"{_httpContextAccessor.HttpContext.Request.Scheme}://{_httpContextAccessor.HttpContext.Request.Host}";
+            var routeforDb = Path.Combine(url, container, fileName).Replace("\\", "/");
+            return routeforDb;
         }
     }
 }
